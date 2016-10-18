5d252f78a5e5a986977cfe6be63f542e5078e972
Edwin Horneij <edwin@astekwallcovering.com>
2016-10-18 14:55:32 -0700

    Move CSS to separate file

--- lib/generators/spree_sitemap/install/install_generator.rb
+++ lib/generators/spree_sitemap/install/install_generator.rb
@@ -3,6 +3,10 @@ module SpreeSitemap
     class InstallGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../templates', __FILE__)
 
+      def add_stylesheets
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_sitemap\n", :before => /\*\//, :verbose => true
+      end
+
       desc 'Configures your Rails application for use with spree_sitemap_generator'
       def copy_config
         directory 'config'
