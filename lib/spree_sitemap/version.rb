71d253af6e0b614df4e7e7d817b293c553ed9e84
Edwin Horneij <edwin@astekwallcovering.com>
2016-10-19 14:50:16 -0700

    Update version number

--- lib/spree_sitemap/version.rb
+++ lib/spree_sitemap/version.rb
@@ -9,8 +9,8 @@ module SpreeSitemap
 
   module VERSION
     MAJOR = 3
-    MINOR = 0
-    TINY  = 6
+    MINOR = 1
+    TINY  = 0
     PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
