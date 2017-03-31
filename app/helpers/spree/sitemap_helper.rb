ea338a91a9fe8cc851d39e1c0acd0c93ed4718e8
Edwin Horneij <edwin@astekwallcovering.com>
2017-03-31 10:21:29 -0700

    Generate xml sitemap, gzip it, cache it for 12 hours, and send it as a file for download

--- app/helpers/spree/sitemap_helper.rb
+++ app/helpers/spree/sitemap_helper.rb
@@ -1,6 +1,8 @@
 module Spree
   module SitemapHelper
 
+    SITE_ROOT = 'https://www.designyourwall.com'
+
     def taxons_list(parent_taxon, level = 5)
       return '' if level < 1 || parent_taxon.leaf?
       content_tag :ul do
@@ -44,6 +46,76 @@ module Spree
           content_tag(:li, link_to(post.title, blog_post_path(post.slug)))
         end
         safe_join(links, $/) + $/
+      end
+    end
+
+    def get_xml
+      @builder = Builder::XmlMarkup.new
+      @builder.instruct!
+      @builder.urlset ({
+          'xmlns:xsi' => 'http://www.w3.org/2001/XMLSchema-instance',
+          'xmlns' => 'http://www.sitemaps.org/schemas/sitemap/0.9',
+          'xmlns:image' => 'http://www.google.com/schemas/sitemap-image/1.1',
+          'xmlns:video' => 'http://www.google.com/schemas/sitemap-video/1.1',
+          'xmlns:geo' => 'http://www.google.com/geo/schemas/sitemap/1.0',
+          'xmlns:news' => 'http://www.google.com/schemas/sitemap-news/0.9',
+          'xmlns:mobile' => 'http://www.google.com/schemas/sitemap-mobile/1.0',
+          'xmlns:pagemap' => 'http://www.google.com/schemas/sitemap-pagemap/1.0',
+          'xmlns:xhtml' => 'http://www.w3.org/1999/xhtml',
+          'xsi:schemaLocation' => 'http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd'
+      }) do
+        add_homepage
+        add_products
+        add_taxons
+        add_static_pages
+      end
+      @builder.target!
+    end
+
+
+    def add_homepage
+      xml_url SITE_ROOT, Time.now, 'always', 1.0
+    end
+
+
+    def add_taxons
+      Spree::Taxon.roots.each { |taxon| add_taxon(taxon) }
+    end
+
+    def add_taxon taxon
+      xml_url SITE_ROOT+nested_taxons_path(taxon.permalink), taxon.products.last_updated if taxon.permalink.present?
+      taxon.children.each { |child| add_taxon child }
+    end
+
+
+    def add_products
+      xml_url SITE_ROOT+products_path, active_products.last_updated
+      active_products.each { |product| xml_url SITE_ROOT+product_path(product), product.updated_at }
+    end
+
+    def active_products
+      Spree::Product.active.uniq
+    end
+
+
+    def add_static_pages
+      static_pages.each { |url| xml_url SITE_ROOT+url }
+    end
+
+    def static_pages
+      %w[/contact-us /help /help/returns /help/shipping /help/faq /help/privacy-and-security-policies
+            /help/terms-and-conditions /help/how-hang-wallpaper /help/wallpaper-instructions /help/wall-murals-instructions
+            /help/grasscloth-wallpaper-instructions /help/glass-beaded-wallpaper-instructions
+            /help/flock-wallpaper-instructions /help/hanging-wallpaper /help/dictionary /design-your-own /custom_tips]
+    end
+
+
+    def xml_url loc, lastmod = nil, changefreq = nil, priority = nil
+      @builder.url do
+        @builder.loc loc
+        @builder.lastmod (lastmod.nil? ? Time.now : lastmod).strftime '%Y-%m-%dT%H:%M:%S%:z'
+        @builder.changefreq (changefreq.nil? ? 'weekly' : changefreq)
+        @builder.priority (priority.nil? ? 0.5 : priority)
       end
     end
 
